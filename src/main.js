5d9c04d4eebace1f5c044a65b8b6ec1c74fdc585
GR <gaoruiai521@gmail.com>
2019-01-30 15:37:47 +0800

    Global fontsize and language

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 
+import Cookies from 'js-cookie'
+
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
@@ -16,6 +18,7 @@ import '@/icons' // icon
 import '@/permission' // permission control
 
 Vue.use(ElementUI, {
+  size: Cookies.get('size') || 'small', // set element-ui default size
   i18n: (key, value) => i18n.t(key, value)
 })
 
